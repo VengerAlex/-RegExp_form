2a6ee8076515a7997c15128a9466c4fa5b2848e3
VengerAlex <alexvenger90@gmail.com>
2021-07-12 17:10:47 +1200

    finished with js

--- js.js
+++ js.js
@@ -1,19 +1,73 @@
 const inputs = document.querySelectorAll('input');
 
-
 const patterns = {
 	telephone: /^\d{10}$/,
-	slug: /^([a-z0-9]-){11-20}$/,
-};
-
-const validateFunc = (field, regEx) => {
-	console.log(regEx.test(field.value));
+	username: /^[a-z\d]{5,12}$/i,
+	password: /^[\w@-]{8,20}$/,
+	slug: /^[a-z\d-]{8,20}$/,
+	email: /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,10})?$/
 }
 
-inputs.forEach(input => {
-	input.addEventListener('keyup', (e) => {
-		// console.log(e.target.attributes.name.value);
+const validateFunc = (field, regEx) => {
+	regEx.test(field.value) ? field.className = 'valid' : field.className = 'invalid';
+}
+
+inputs.forEach(el => {
+	el.addEventListener('keyup', (e) => {
 
 		validateFunc(e.target, patterns[e.target.attributes.name.value]);
-	})
-})
+	});
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const inputs = document.querySelectorAll('input');
+
+
+// const patterns = {
+// 	telephone: /^\d{10}$/,
+// 	slug: /^([a-z0-9]-){11-20}$/,
+// };
+
+// const validateFunc = (field, regEx) => {
+	// console.log(regEx.test(field.value));
+
+// 	if (regEx.test(field.value)) {
+// 		field.className = 'valid'
+// 	} else {
+// 		field.className = 'ivalid'
+// 	}
+// }
+
+// inputs.forEach(input => {
+// 	input.addEventListener('keyup', (e) => {
+// 		// console.log(e.target.attributes.name.value);
+
+// 		validateFunc(e.target, patterns[e.target.attributes.name.value]);
+// 	})
+// })
