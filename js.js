c48559db646522c1e04958b6857be54cab57d1c6
VengerAlex <alexvenger90@gmail.com>
2021-07-12 16:14:51 +1200

    check if the telephone is valid

--- js.js
+++ js.js
@@ -1,7 +1,19 @@
+const inputs = document.querySelectorAll('input');
 
 
+const patterns = {
+	telephone: /^\d{10}$/,
+	slug: /^([a-z0-9]-){11-20}$/,
+};
 
+const validateFunc = (field, regEx) => {
+	console.log(regEx.test(field.value));
+}
 
+inputs.forEach(input => {
+	input.addEventListener('keyup', (e) => {
+		// console.log(e.target.attributes.name.value);
 
-
-const reg = /[a-z]/ig;
+		validateFunc(e.target, patterns[e.target.attributes.name.value]);
+	})
+})
